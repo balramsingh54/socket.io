610300358c59f1033c0518d974fe8da1d663765c
Balram Kumar Singh <41859769+balramsingh54@users.noreply.github.com>
2021-07-24 17:19:01 +0530

    broadcasting

--- app.js
+++ app.js
@@ -6,19 +6,16 @@ app.get('/', function(req, res) {
    res.sendfile('index.html');
 });
 
+var clients = 0;
 io.on('connection', function(socket) {
-   console.log('A user connected');
-
-   //Send a message after a timeout of 4seconds
-   setTimeout(function() {
-      socket.send('Sent a message 4seconds after connection!');
-   }, 4000);
-
+   clients++;
+   io.sockets.emit('broadcast',{ description: clients + ' clients connected!'});
    socket.on('disconnect', function () {
-      console.log('A user disconnected');
+      clients--;
+      io.sockets.emit('broadcast',{ description: clients + ' clients connected!'});
    });
 });
 
 http.listen(3000, function() {
-   console.log('listening on *:3000');
+   console.log('listening on localhost:3000');
 });
